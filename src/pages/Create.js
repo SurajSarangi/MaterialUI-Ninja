44cb20befe18ada387780d923bd8849cbb2c3690
SurajSarangi <surajsarangi1@gmail.com>
2021-04-10 19:34:17 +0530

    added icon to create button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,7 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import SendIcon from '@material-ui/icons/Send';
 
 export default function Create() {
 	return (
@@ -19,6 +20,7 @@ export default function Create() {
 				variant="contained"
 				color="secondary"
 				type="submit"
+				endIcon={<SendIcon />}
 			>
 				Create
 			</Button>
