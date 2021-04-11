1674942e13e8920ff262f37e98ef342395516e1c
SurajSarangi <surajsarangi1@gmail.com>
2021-04-11 12:55:55 +0530

    created textfields in form and used states

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,12 +2,42 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import SendIcon from '@material-ui/icons/Send';
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import { useState } from 'react';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+	field: {
+		marginTop: 20,
+		marginBottom: 20,
+		display: 'block'
+	  }
+});
 
 export default function Create() {
 	const classes = useStyles();
+
+	const [title, setTitle] = useState('');
+	const [note, setNote] = useState('');
+	const [titleError, setTitleError] = useState(false);
+	const [noteError, setNoteError] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setTitleError(false);
+		setNoteError(false);
+
+		if(title === '') {
+			setTitleError(true);
+		}
+		if(note === '') {
+			setNoteError(true);
+		}
+
+		if(title && note){
+			console.log(title, note);
+		}
+	}
 
 	return (
 		<Container>
@@ -20,15 +50,38 @@ export default function Create() {
 				Create a New Note
 			</Typography>
 			
-			<Button 
-				onClick={() => console.log('Clicked')}
-				variant="contained"
-				color="secondary"
-				type="submit"
-				endIcon={<SendIcon />}
-			>
-				Create
-			</Button>
+			<form noValidate autoComplete="off" onSubmit={handleSubmit}>
+				<TextField
+					onChange={(e) => setTitle(e.target.value)}
+					className={classes.field}
+					label="Title"
+					variant="outlined"
+					fullWidth
+					required
+					error={titleError}
+				/>
+				<TextField
+					onChange={(e) => setNote(e.target.value)}
+					className={classes.field}
+					label="Note"
+					variant="outlined"
+					multiline
+					rows = {4}
+					fullWidth
+					required
+					error={noteError}
+				/>
+
+				<Button 
+					variant="contained"
+					color="secondary"
+					type="submit"
+					endIcon={<SendIcon />}
+				>
+					Create
+				</Button>
+			</form>
+
 		</Container>
 	)
 }
